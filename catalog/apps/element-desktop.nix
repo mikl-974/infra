abc4a12fbc6dbb6e9ae079dffd55779358eb6a4c
Mickael <mickaael.folio@gmail.com>
2026-06-14 20:43:42 +0700

    feat: refactor element-desktop configuration to use symlinkJoin

--- catalog/apps/element-desktop.nix
+++ catalog/apps/element-desktop.nix
@@ -1,4 +1,12 @@
 { pkgs }:
-with pkgs; [
-  element-desktop
+[
+  (pkgs.symlinkJoin {
+    name = "element-desktop";
+    paths = [ pkgs.element-desktop ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/element-desktop \
+        --add-flags "--password-store=basic"
+    '';
+  })
 ]
